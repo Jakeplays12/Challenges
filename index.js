d89924ee8039e6a19dafaa75cbc2d629fd583e0c
Jakeplays12 <96874701+Jakeplays12@users.noreply.github.com>
2022-06-06 07:03:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-console.log("Gay")
+console.log("\n5 X 5 Square.\n")
 
 var i;
   
 for (i = 0; i < 5; i++) 
 {
     console.log("* * * * * ");
+}
+
+console.log("\n10 X 10 Square.\n")
+
+var i;
+  
+for (i = 0; i < 10; i++) 
+{
+    console.log("* * * * * * * * * *");
 }
