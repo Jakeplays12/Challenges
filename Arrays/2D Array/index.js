3a21a781f2fd4d5c4cb2ed4a0723e49ddded43fd
Jakeplays12 <96874701+Jakeplays12@users.noreply.github.com>
2022-09-27 15:45:53 +0100

    Create index.js

---
+++ Arrays/2D Array/index.js
@@ -0,0 +1,9 @@
+let ArrayF = Array ( )
+
+ArrayF[0] = Array (1, 2, 3, 4, 5)
+ArrayF[1] = Array (6, 7, 8, 9, 10)
+ArrayF[2] = Array (11, 12, 13, 14, 15)
+ArrayF[3] = Array (16, 17, 18, 19, 20)
+ArrayF[4] = Array (21, 22, 23, 24, 25)
+
+console.log(ArrayF)
