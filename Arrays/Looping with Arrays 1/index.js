a1b13321f98057d15f9f5b47502d4a3ad4ca1e8b
Jakeplays12 <96874701+Jakeplays12@users.noreply.github.com>
2022-09-27 14:46:05 +0100

    Create index.js

---
+++ Arrays/Looping with Arrays 1/index.js
@@ -0,0 +1,12 @@
+let ArrayF = Array ( )
+
+ArrayF[0] = Array (1, 2, 3, 4, 5)
+
+for (let i = 0; i < 3; i++) {
+let I2 = (i + 1)
+let N1 = [(ArrayF[i][Array.length + 3] + 1), (ArrayF[i][Array.length + 3] + 2), (ArrayF[i][Array.length + 3] + 3), (ArrayF[i][Array.length + 3] + 4), (ArrayF[i][Array.length + 3] + 5)]
+
+ArrayF[I2] = N1
+
+}
+console.log(`\n - - - - - \n`, ArrayF, `\n - - - - - \n`, )
