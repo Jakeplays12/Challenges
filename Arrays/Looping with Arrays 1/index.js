0347958c404d3efd8cf4be2241b1731254716ddc
Jakeplays12 <96874701+Jakeplays12@users.noreply.github.com>
2022-09-27 14:50:28 +0100

    Update index.js

--- Arrays/Looping with Arrays 1/index.js
+++ Arrays/Looping with Arrays 1/index.js
@@ -1,8 +1,14 @@
+// Looping Config
+
+var Loop_Amount = 3
+
+// Code
+
 let ArrayF = Array ( )
 
 ArrayF[0] = Array (1, 2, 3, 4, 5)
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < Loop_Amount; i++) {
 let I2 = (i + 1)
 let N1 = [(ArrayF[i][Array.length + 3] + 1), (ArrayF[i][Array.length + 3] + 2), (ArrayF[i][Array.length + 3] + 3), (ArrayF[i][Array.length + 3] + 4), (ArrayF[i][Array.length + 3] + 5)]
 
